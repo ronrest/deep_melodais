fae4cbb227d58687cd493ce271424be596d2629c
ronrest <ronny.coding@gmail.com>
2017-06-24 22:06:20 +1000

    DOCS: Add main.py with description

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+DEEP MELODAIS
+- An AI based on deep neural networks that that composes music.
+- uses an LSTM for generating monophonic music as MIDI instructions.
+
+Basing this on the work described in the following places:
+- http://yoavz.com/music_rnn/
+- https://www.youtube.com/watch?v=S_f2qV2_U00
+
+"""
+
+DATA_DIR = "data"
